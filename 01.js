415f6d7cd205ac4d1de3df4e86dfbb2ceea53342
Victor <victor@victor.com>
2020-09-02 12:13:33 +0200

    primeros pasos vue

---
+++ 01.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello Vue',
+        fruits: [
+            {
+                name: 'apple',
+                qty: 0
+            },
+            {
+                name: 'pear',
+                qty:  12           
+            }]
+    }
+})
