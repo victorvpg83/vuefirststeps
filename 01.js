a1c6fd0b3b1fd88fac87611f4029e016009d4789
Victor <victor@victor.com>
2020-09-02 17:33:09 +0200

    principios vue

--- 01.js
+++ 01.js
@@ -2,14 +2,42 @@ const app = new Vue({
     el: '#app',
     data: {
         title: 'Hello Vue',
-        fruits: [
-            {
-                name: 'apple',
-                qty: 0
-            },
-            {
-                name: 'pear',
-                qty:  12           
-            }]
+        fruits: 
+            [ 
+                { name: 'apple', qty: 0 },
+                { name: 'pear', qty:  12 }
+            ],
+        background: 'bg-warning',
+        color: false,
+        message: 'Hola Víctor',
+        newFruit: '',
+        total: 0,
+        counter: 0
+    },
+    methods: {
+        addFruit() {
+            this.fruits.push({ name: this.newFruit, qty: 0 })
+            this.newFruit = ''
+        }
+    },
+    computed: {
+        sumFruits() {
+            this.total = 0
+            for( fruit of this.fruits ) {
+                this.total = this.total + fruit.qty
+            }
+            return this.total
+        },
+        inverted() {
+            return this.message.split('').reverse().join('')
+        },
+        color2() {
+            return {
+                'bg-success': this.counter <= 10,
+                'bg-warning': this.counter > 10 && this.counter <=20,
+                'bg-danger': this.counter > 20
+            }
+        }
     }
+
 })
